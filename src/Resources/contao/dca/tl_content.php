d0f719798d44da5ab8a7402ac965d940df1d382c
Niels <info@heimseiten.de>
2021-08-18 14:48:24 +0200

    added class for backend view

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -8,7 +8,7 @@ use Contao\DataContainer;
 $GLOBALS['TL_DCA']['tl_content']['fields']['noColumn'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_content']['noColumn'],
     'inputType' => 'checkbox', 
-    'eval'      => array('tl_class' => 'w50'),
+    'eval'      => array('tl_class' => 'w50 m12'),
     'sql'       => "char(1) NOT NULL default ''" 
 ];
 
